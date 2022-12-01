1a2602637fd073320ae3707cae8a4666f0665a55
dorsy99 <andrewalburydor@gmail.com>
2022-12-01 16:08:25 +1000

    day1_2 done.

---
+++ day1/day1_2.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+var filename = "day1/input.txt";
+
+fs.readFile(filename, 'utf8', function(err, data){
+    if (err) throw err;
+    var dataInt = []; 
+    data.split("\r\n").forEach(function(i){
+        dataInt.push(i);
+    });
+    runIt(dataInt);
+})
+
+var runIt = function(data) {
+    var elves = [];
+    var curElf = 0;
+    data.forEach(element => {
+        if (element !== '') {
+            curElf = curElf + (parseInt(element))
+        } else {
+            elves.push(curElf);
+            curElf = 0;
+        }
+    });
+    
+    console.log(elves)
+    elves.sort((a,b) => a - b)
+    var top3 = elves.slice((elves.length -3), (elves.length))
+
+    var top3total = 0;
+    console.log(elves)
+    top3.forEach(element => {
+        top3total = top3total + element;
+    });
+    
+    console.log(top3total)
+}
